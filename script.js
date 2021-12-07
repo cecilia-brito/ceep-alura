135bfb0a994ba0eb101a51ab09c5db15d66c2ea2
Cecilia Brito Santos <ceciliabritosantos@gmail.com>
2021-12-07 14:24:25 -0300

    used IIFE to hide business rule

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-const newTask = document.querySelector('[data-form-button]');
-const createTask = (event) => {
-    event.preventDefault() 
-    const list = document.querySelector('[data-list]')
-    const inputNewTask = document.querySelector('[data-form-input]');
-    const valueInput = inputNewTask.value;
-    console.log(valueInput); 
-    const task = document.createElement('li');
-    task.classList.add('task')
-    const content = `<p class='content'>${valueInput}</p>`
-    
-    task.innerHTML = content;
-    list.appendChild(task)
-    valueInput = ' ';
-}
+(() =>{
+    const newTask = document.querySelector('[data-form-button]');
+    const createTask = (event) => {
+        event.preventDefault() 
+        const list = document.querySelector('[data-list]')
+        const inputNewTask = document.querySelector('[data-form-input]');
+        const valueInput = inputNewTask.value;
+        console.log(valueInput); 
+        const task = document.createElement('li');
+        task.classList.add('task')
+        const content = `<p class='content'>${valueInput}</p>`
+        
+        task.innerHTML = content;
+        list.appendChild(task)
+        task.appendChild(completeBtn())
+        valueInput = ' ';
+    }
 
-newTask.addEventListener('click', createTask)
+    newTask.addEventListener('click', createTask)
+
+    const completeBtn = () =>{
+        const completeButton = document.createElement('button')
+        completeButton.classList.add('check-button')
+        completeButton.innerText = "concluir"
+        completeButton.addEventListener('click', completeTask)
+
+        return completeButton;
+    }
+
+    const completeTask = (event) => {
+    const btnCompleteFunction = event.target
+    const taskComplete = btnCompleteFunction.parentElement;
+
+    taskComplete.classList.toggle('done')
+    }
+})()
